97432b33ed23730362ec66590284e6e02d970320
zhuchi76 <tzuchichen.en12@nycu.edu.tw>
2024-12-18 17:16:55 +0800

    feat(build): add symlink-install to ROS build scripts
    - Updated `build_ros1_all.sh` to use `--symlink-install` with `catkin build`
    - Created `build_ros2_all.sh` for building ROS2 workspace with `colcon build`
      and `--symlink-install`
    - Updated `build_ros2_camera.sh` to use `--symlink-install` with `colcon build`
    - Created `build_ros2_pcd.sh` for building pointcloud processing package with
      `colcon build` and `--symlink-install`

--- build_ros2_pcd.sh
+++ build_ros2_pcd.sh
@@ -3,7 +3,7 @@
 export ROS2_INSTALL_PATH=/opt/ros/humble
 
 cd ros2_ws
-colcon build --packages-select realsense2_camera_example --symlink-install
+colcon build --packages-select pointcloud_processing --symlink-install
 cd ..
 
 source ${ROS2_INSTALL_PATH}/setup.bash
